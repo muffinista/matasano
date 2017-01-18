96884ba01486fb772393a37bbd52b12427a4b733
Colin Mitchell <muffinista@gmail.com>
2017-01-18 17:21:11 -0500

    Use cycle iterator

--- src/encode.rs
+++ src/encode.rs
@@ -5,15 +5,11 @@ pub fn xor_encode(src: &str, key: &str) -> String {
     let key = String::from(key).into_bytes();
     let mut out = String::from(src).into_bytes();
     
-    let mut key_index = 0;
+    let mut iter = key.iter().cycle();
     for i in 0..out.len() {
-        out[i] ^= key[key_index];
-        key_index +=1;
-        if key_index >= key.len() {
-            key_index = 0;
-        }
+        out[i] ^= *iter.next().unwrap();
     }
-    //String::from_utf8(out).unwrap().to_hex()
+
     out.to_hex()
 }
     
