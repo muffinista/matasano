eca21d1425e0effe0b7c92b6ae4b10b8b4237c2f
Colin Mitchell <muffinista@gmail.com>
2017-03-23 21:46:13 -0400

    Update xor_encode to use arrays

--- src/encode.rs
+++ src/encode.rs
@@ -1,15 +1,12 @@
-extern crate rustc_serialize;
-use rustc_serialize::hex::{ToHex};
 
-pub fn xor_encode(src: &str, key: &str) -> String {
-    let key = String::from(key).into_bytes();
-    let mut out = String::from(src).into_bytes();
+pub fn xor_encode(src: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut out:Vec<u8> = Vec::new();
     
     let mut iter = key.iter().cycle();
-    for i in 0..out.len() {
-        out[i] ^= *iter.next().unwrap();
+    for i in 0..src.len() {
+        out.push(src[i] ^ iter.next().unwrap());
     }
 
-    out.to_hex()
+    out
 }
     
--- src/set1_5.rs
+++ src/set1_5.rs
@@ -29,16 +29,18 @@ mod test {
     
     use encode;
     
+    extern crate rustc_serialize;
+    use rustc_serialize::hex::{ToHex};
     
     #[test]
     fn test_c05() {
         let src = "Burning 'em, if you ain't quick and nimble\n\
-                   I go crazy when I hear a cymbal";
-        let key = "ICE";
+                   I go crazy when I hear a cymbal".as_bytes();
+        let key = "ICE".as_bytes();
         let expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272\
                         a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
 
-        let encoded = encode::xor_encode(src, key);
+        let encoded = encode::xor_encode(src, key).to_hex();
         
 
         assert_eq!(expected, encoded);
